354dd0d75a8dfdac70fa70f29f2788a3f948f858
kajimost <templetrader@gmail.com>
2026-09-11 14:24:05 +0300

    Mini Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import './App.css'
 import { Store } from "./components/Store/Store";
 
 function App() {
-  return (
-    <>
-      <Store />
-    </>
-  )
+  return <Store />
 }
 
 export default App
